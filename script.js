ebd399c56b1c897d295bd9eb0f81e1a8bfe84b29
Kalvarado16 <43443746+Kalvarado16@users.noreply.github.com>
2019-01-03 09:49:28 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".camera1").click(function(){
+$(".BrooklynB").fadeToggle();
+});
+
+$(".camera2").click(function(){
+$(".EmpireS").fadeToggle();
+});
+
+$(".camera1").click(function(){
+$(".BrooklynB").fadeToggle();
+});
